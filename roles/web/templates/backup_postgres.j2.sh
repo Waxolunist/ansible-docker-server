24712eb3c60efaab974a7230c1f908b1a971015d
Christian Sterzl <christian.sterzl@gmail.com>
2021-02-06 21:26:39 +0100

    labels-be
    Signed-off-by: Christian Sterzl <christian.sterzl@gmail.com>

---
+++ roles/web/templates/backup_postgres.j2.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+
+docker-compose exec grafana-pg pg_dump -c -U '{{ vault_grafana_pg_user }}' | gzip -9 > /backup/dump_grafana_`date +%d-%m-%Y"_"%H_%M_%S`.sql.zip
+
+docker-compose exec portus-pg pg_dump -c -U '{{ vault_portus_pg_user }}'  | gzip -9 > /backup/dump_portus_`date +%d-%m-%Y"_"%H_%M_%S`.sql.zip
